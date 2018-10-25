84e2b8e5fc48e65229f14e19e6a9e69db67037d4
Chris Carlevato <chris@christopherl.com>
2018-10-24 20:45:24 -0700

    Add version update

--- utm-dot-codes.php
+++ utm-dot-codes.php
@@ -10,7 +10,7 @@
  * Plugin Name: utm.codes
  * Plugin URI: https://utm.codes
  * Description: Create and manage your marketing campaign links in WordPress.
- * Version: 1.2
+ * Version: 1.3.0
  * Author: ChristopherL
  * Author URI: https://christopherl.com
  * License: GPL v2
