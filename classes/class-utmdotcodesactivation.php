9e2e81fd761b8d476c81f886a4e24171e88b59b8
Chris Carlevato <chris@christopherl.com>
2018-11-04 21:40:33 -0800

    Add cleanup to doc blocks

--- classes/class-utmdotcodesactivation.php
+++ classes/class-utmdotcodesactivation.php
@@ -17,7 +17,6 @@ class UtmDotCodesActivation {
 	/**
 	 * UtmDotCodes_Activation constructor, adds (de)activation hooks for our plugin
 	 *
-	 * @version 1.0
 	 * @since 1.0
 	 */
 	public function __construct() {
@@ -28,7 +27,6 @@ class UtmDotCodesActivation {
 	/**
 	 * Processes plugin activation, including version checks
 	 *
-	 * @version 1.0
 	 * @since 1.0
 	 */
 	public function activation() {
