fec2a28fed631de55cbe871118eae02ae659762f
Chris Carlevato <chris@asdf.dev>
2020-12-24 12:46:09 -0800

    Fix build notification

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
+const path = require('path');
+
 module.exports = require('@flickerbox/build/webpack.config')
   .output
     .path(__dirname)
     .filename('js/[name].js')
+    .end()
+  .plugin('notifier')
+    .use(require('webpack-notifier'), [{
+      title: 'utm.codes Build',
+      contentImage: null,
+      alwaysNotify: true,
+      skipFirstNotification: false,
+      excludeWarnings: false,
+    }])
     .end()
   .entry('utmdotcodes')
     .add('./_build/styles/utmdotcodes.scss')
