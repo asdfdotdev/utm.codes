e1bac424652fb16fa184f261e2e261db298f1d24
Chris Carlevato <chris@asdf.dev>
2025-04-09 19:52:35 -0400

    Change bitly test to match rebrandly
    Free account limits...

--- _test/tests/TestUtmDotCodesShortenBitly.php
+++ _test/tests/TestUtmDotCodesShortenBitly.php
@@ -10,6 +10,10 @@
  */
 class TestUtmDotCodesShortenBitly extends WP_UnitTestCase {
 
+	function should_do_test() {
+		return (getenv( 'UTMDC_BITLY_DO_TESTS' ) === 'true');
+	}
+
 	/**
 	 * Confirm our plugin knows we're testing.
 	 */
@@ -25,13 +29,21 @@ class TestUtmDotCodesShortenBitly extends WP_UnitTestCase {
 	 * @depends test_is_test
 	 */
 	function test_version_numbers_active() {
-		$is_valid_wp = version_compare( get_bloginfo( 'version' ), UTMDC_MINIMUM_WP_VERSION, '>' );
-		$this->assertTrue( $is_valid_wp );
+		if ($this->should_do_test()) {
+			$is_valid_wp = version_compare( get_bloginfo( 'version' ), UTMDC_MINIMUM_WP_VERSION, '>' );
+			$this->assertTrue( $is_valid_wp );
 
-		$is_valid_php = version_compare( phpversion(), UTMDC_MINIMUM_PHP_VERSION, '>' );
-		$this->assertTrue( $is_valid_php );
+			$is_valid_php = version_compare( phpversion(), UTMDC_MINIMUM_PHP_VERSION, '>' );
+			$this->assertTrue( $is_valid_php );
 
-		$this->assertTrue( is_plugin_active( 'utm-dot-codes/utm-dot-codes.php' ) );
+			$this->assertTrue( is_plugin_active( 'utm-dot-codes/utm-dot-codes.php' ) );
+		} else {
+			if (method_exists( $this, 'expectNotToPerformAssertions' )) {
+				$this->expectNotToPerformAssertions();
+			} else {
+				$this->assertTrue( true );
+			}
+		}
 	}
 
 	/**
@@ -40,23 +52,31 @@ class TestUtmDotCodesShortenBitly extends WP_UnitTestCase {
 	 * @depends test_is_test
 	 */
 	function test_bitly_request() {
-		require_once getenv( 'UTMDC_PLUGIN_DIR' ) . '/classes/shorten/interface.php';
-		require_once getenv( 'UTMDC_PLUGIN_DIR' ) . '/classes/shorten/class-bitly.php';
+		if ($this->should_do_test()) {
+			require_once getenv( 'UTMDC_PLUGIN_DIR' ) . '/classes/shorten/interface.php';
+			require_once getenv( 'UTMDC_PLUGIN_DIR' ) . '/classes/shorten/class-bitly.php';
 
-		$plugin = new UtmDotCodes();
+			$plugin = new UtmDotCodes();
 
-		$shortener = new \UtmDotCodes\Bitly(
-			getenv( 'UTMDC_BITLY_API' )
-		);
+			$shortener = new \UtmDotCodes\Bitly(
+				getenv( 'UTMDC_BITLY_API' )
+			);
 
-		$shortener->shorten(
-			[ 'utmdclink_url' => 'https://www.' . uniqid() . '.test' ],
-			'?test=1234'
-		);
+			$shortener->shorten(
+				[ 'utmdclink_url' => 'https://www.' . uniqid() . '.test' ],
+				'?test=1234'
+			);
 
-		$this->assertTrue( $shortener instanceof \UtmDotCodes\Shorten );
-		$this->assertEquals( null, $shortener->get_error() );
-		$this->assertTrue( strpos( $plugin->null_string_check( $shortener->get_response() ), 'https://bit.ly/' ) !== false );
+			$this->assertTrue( $shortener instanceof \UtmDotCodes\Shorten );
+			$this->assertEquals( null, $shortener->get_error() );
+			$this->assertTrue( strpos( $plugin->null_string_check( $shortener->get_response() ), 'https://bit.ly/' ) !== false );
+		} else {
+			if (method_exists( $this, 'expectNotToPerformAssertions' )) {
+				$this->expectNotToPerformAssertions();
+			} else {
+				$this->assertTrue( true );
+			}
+		}
 	}
 
 	/**
@@ -65,23 +85,31 @@ class TestUtmDotCodesShortenBitly extends WP_UnitTestCase {
 	 * @depends test_is_test
 	 */
 	function test_bitly_request_no_api_key() {
-		require_once getenv( 'UTMDC_PLUGIN_DIR' ) . '/classes/shorten/interface.php';
-		require_once getenv( 'UTMDC_PLUGIN_DIR' ) . '/classes/shorten/class-bitly.php';
+		if ($this->should_do_test()) {
+			require_once getenv( 'UTMDC_PLUGIN_DIR' ) . '/classes/shorten/interface.php';
+			require_once getenv( 'UTMDC_PLUGIN_DIR' ) . '/classes/shorten/class-bitly.php';
 
-		$plugin = new UtmDotCodes();
+			$plugin = new UtmDotCodes();
 
-		$shortener = new \UtmDotCodes\Bitly(
-			'this_wont_work'
-		);
+			$shortener = new \UtmDotCodes\Bitly(
+				'this_wont_work'
+			);
 
-		$shortener->shorten(
-			[ 'utmdclink_url' => 'https://www.' . uniqid() . '.test' ],
-			'?test=1234'
-		);
+			$shortener->shorten(
+				[ 'utmdclink_url' => 'https://www.' . uniqid() . '.test' ],
+				'?test=1234'
+			);
 
-		$this->assertTrue( $shortener instanceof \UtmDotCodes\Shorten );
-		$this->assertEquals( 4030, $shortener->get_error() );
-		$this->assertTrue( strpos( $plugin->null_string_check( $shortener->get_response() ), 'http://bit.ly/' ) === false );
+			$this->assertTrue( $shortener instanceof \UtmDotCodes\Shorten );
+			$this->assertEquals( 4030, $shortener->get_error() );
+			$this->assertTrue( strpos( $plugin->null_string_check( $shortener->get_response() ), 'http://bit.ly/' ) === false );
+		} else {
+			if (method_exists( $this, 'expectNotToPerformAssertions' )) {
+				$this->expectNotToPerformAssertions();
+			} else {
+				$this->assertTrue( true );
+			}
+		}
 	}
 
 	/**
@@ -90,23 +118,31 @@ class TestUtmDotCodesShortenBitly extends WP_UnitTestCase {
 	 * @depends test_is_test
 	 */
 	function test_bitly_request_no_link() {
-		require_once getenv( 'UTMDC_PLUGIN_DIR' ) . '/classes/shorten/interface.php';
-		require_once getenv( 'UTMDC_PLUGIN_DIR' ) . '/classes/shorten/class-bitly.php';
+		if ($this->should_do_test()) {
+			require_once getenv( 'UTMDC_PLUGIN_DIR' ) . '/classes/shorten/interface.php';
+			require_once getenv( 'UTMDC_PLUGIN_DIR' ) . '/classes/shorten/class-bitly.php';
 
-		$plugin = new UtmDotCodes();
+			$plugin = new UtmDotCodes();
 
-		$shortener = new \UtmDotCodes\Bitly(
-			getenv( 'UTMDC_BITLY_API' )
-		);
+			$shortener = new \UtmDotCodes\Bitly(
+				getenv( 'UTMDC_BITLY_API' )
+			);
 
-		$shortener->shorten(
-			[ 'utmdclink_url' => '' ],
-			''
-		);
+			$shortener->shorten(
+				[ 'utmdclink_url' => '' ],
+				''
+			);
 
-		$this->assertTrue( $shortener instanceof \UtmDotCodes\Shorten );
-		$this->assertEquals( 500, $shortener->get_error() );
-		$this->assertTrue( strpos( $plugin->null_string_check( $shortener->get_response() ), 'http://bit.ly/' ) === false );
+			$this->assertTrue( $shortener instanceof \UtmDotCodes\Shorten );
+			$this->assertEquals( 500, $shortener->get_error() );
+			$this->assertTrue( strpos( $plugin->null_string_check( $shortener->get_response() ), 'http://bit.ly/' ) === false );
+		} else {
+			if (method_exists( $this, 'expectNotToPerformAssertions' )) {
+				$this->expectNotToPerformAssertions();
+			} else {
+				$this->assertTrue( true );
+			}
+		}
 	}
 
 }
